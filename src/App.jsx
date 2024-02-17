f80c9b78c2e43091b860a66df12d7980fe00f93a
Ayush Kansal <ayushkansal303@gmail.com>
2024-02-18 03:19:59 +0530

    added login and feedback component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
 import React from 'react'
 import NewNav from './components/NewNav'
-import abstract from './assets/homeAbs.svg'
-// import Navbar from './components/Navbar'
-// import Login from './routes/Login'
+import Header from './components/Header'
 
-// import Navbar2 from './components/Navbar2';
+import Login from './routes/NewLogin'
+import Feedback from './routes/Feedback'
 
 function App() {
-  return (
+  return (    
     <>
-      {/* <Navbar />
-      <div className='flex'>
-      <Navbar2 />
-      <Login />
-      </div> */}
-
       <NewNav />
-
-      <div className='flex justify-evenly items-center w-full h-93vh'>
-        <div className="flex flex-col justify-start gap-5 h-[93vh] items-center basis-1/2">
-          <p className='text-5xl font-bold w-[60%]'>Explore Domains!</p>
-          
-          <input className='h-6 border-black bg-gray-200 w-[60%] ' type="text" />
-        </div>
-        <div className='flex justify-center items-center h-[93vh]   basis-1/2'>
-          <img className='w-[550px]' src={abstract} alt="" placeholder="Search here!" />
-        </div>
-      </div>
-
-
+      {/* <Header /> */}
+      {/* <Login /> */}
+      <Feedback />
     </>
   )
 }
