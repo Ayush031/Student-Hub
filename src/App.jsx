b3b6955a4ae8af2f089d800854928ba4539f53f8
Ayush Kansal <ayushkansal303@gmail.com>
2024-02-17 19:50:56 +0530

    navbar 2 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Navbar2 from './components/Navbar2';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Navbar2 />
     </>
   )
 }
